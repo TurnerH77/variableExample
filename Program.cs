0ab39bd60cb04f9c0a10eb604f96d29f5352d7a1
turner harris <turnerharris7798@yahoo.com>
2020-08-21 13:43:39 -0500

    created variableEx program

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace variablesEX
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 22;
+            string name = "Turner";
+            double height = 5.11;
+            decimal weight = 190.5m;
+            char initial = 'T';
+            bool isDope = true;
+            Console.WriteLine("Hi! My name is " + name + " and I am " + age + " years old! I am standing at about " + height + " feet tall and currently weight about " + weight + ". \n My initial is " + initial + " and am i dope? " + "is Turner dope?: " + isDope + "." +" i guess so huh.");
+        }
+    }
+}
